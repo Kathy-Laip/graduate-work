e8b174913eb3dfa27b52ff9b4c38de780bbf2f90
Kathy_Laip <ekaterinaslapnikova@Ekaterinas-MacBook-Air.local>
2024-03-14 17:39:27 +0300

    добавлена функция по добыче данных о расписании, пока протестирована только часть университета по вставке данных

--- frontend/backend/main.py
+++ frontend/backend/main.py
@@ -115,6 +115,22 @@ def getTypePlaceID(place):
     except:
         return False
 
+def getGrafic(workID):
+    try: 
+        graifc_mas = connection.get_data_from_table('select time_id, start, end from grafic where sch_id={}'.format(workID))
+        if(graifc_mas is not None):
+            return graifc_mas
+    except:
+        return False
+
+def getSchedule(workID):
+    try:
+        schedules = connection.get_data_from_table('SELECT schedules.schedule.sch_id, schedules.schedule.work_id, schedules.grafic.start, schedules.grafic.end, schedules.place.place_name, schedules.teacher.fio, schedules.teacher_classes.name_sub, schedules.weeks.week_day, schedules.schedule.start_date, schedules.schedule.end_date FROM schedules.schedule INNER JOIN schedules.grafic ON schedules.schedule.grafic_id = schedules.grafic.time_id INNER JOIN schedules.place ON schedule.place_id = schedules.place.place_id INNER JOIN schedules.teacher ON schedule.teacher_id = schedules.teacher.teacher_id INNER JOIN schedules.teacher_classes ON schedules.teacher.teacher_id = schedules.teacher_classes.id_teacher INNER JOIN schedules.weeks on schedules.weeks.id_week = schedules.schedule.week_day where schedules.schedule.work_id = {};'.format(workID))
+        if(schedules is not None):
+            return schedules
+    except: 
+        return False
+
 
 def insertNewUser(login, password):
     ans = connection.execute_query('insert into users(login, password) values("{}", "{}")'.format(login, password))
@@ -172,6 +188,10 @@ def addPlaceUni(workID, place_name, weekID, typeID, count_seat, start, end):
     ans = connection.execute_query('insert into place(work_id, place_name, week_day, type_place, count_seat, start_work, end_work) values({}, "{}", {}, {}, {}, "{}", "{}")'.format(workID, place_name, weekID, typeID, count_seat, start, end))
     return ans
 
+def addScheduleUniDIR(workID, graficID, placeID, teacherID, directionID, week_day, start, end):
+    ans = connection.execute_query('insert into schedule(work_id, grafic_id, place_id, teacher_id, direction_id, week_day, start_date, end_date) values({}, {}, {}, {}, {}, {}, "{}", "{}")'.format(workID, graficID, placeID, teacherID, directionID, week_day, start, end))
+    return ans
+
 @app.route("/", methods=["POST"])
 def signInUser():
     info = json.loads(request.get_data())
@@ -183,37 +203,74 @@ def signInUser():
 
 if __name__ == '__main__':
     # app.run(debug=True, host="127.0.0.1", port="5000")
+    # новый пользователь
+    # print(insertNewUser('ya.shl@yandex.ru', 'kathy_laip123'))
+    userID = getUser('ya.shl@yandex.ru', 'kathy_laip123')
 
-    # print(insertNewUser('yaKatya', '123regopi'))
-    userID = getUser('yaKatya', '123regopi')
-    # typeID = getType('университет')
-    # print(addProject(userID, 'Расписание КФУ ИВМИИТ 2023-2024 1 семестр', '2023-03-19', typeID))
-    workID = getWorkID('Расписание КФУ ИВМИИТ 2023-2024 1 семестр', userID)
-    # print(workID)
+    # добавление нового проекта
+    typeID = getType('университет')
+    # print(addProject(userID, 'Расписание КФУ ИВМИИТ 2023-2024 2 семестр', '2023-01-19', typeID))
+
+    #настройка расписания
+    workID = getWorkID('Расписание КФУ ИВМИИТ 2023-2024 2 семестр', userID)
     # print(addProjectSettings(1,40, '2023-02-09', '2023-05-31', workID))
-    # times = [{'start': '8:30', 'end': '10:10'}, {'start': '10:20', 'end': '11:50'}, {'start': '12:00', 'end': '13:10'}, {'start': '13:30', 'end': '14:10'}, {'start': '14:30', 'end': '15:10'}]
+    times = [{'start': '8:30', 'end': '10:10'}, {'start': '10:20', 'end': '11:50'}, {'start': '12:00', 'end': '13:10'}, {'start': '13:30', 'end': '14:10'}, {'start': '14:30', 'end': '15:10'}]
     # for i in range(len(times)):
     #     print(addTimesGrafic(times[i]['start'], times[i]['end'], workID))
-    # for i in range(1, 5):
+    # for i in range(1, 3):
     #     print(addCourses(i, workID))
-    # print(addCourse(1, workID))
+    
+    # добавление информации про курсы
     course_ID = getCourseID(1, workID)
-    # print(course_ID)
     # print(addDirection(course_ID, 'ФИИТ'))
-    dir_ID = getDirection(course_ID, 'ФИИТ')
-    class_ID = getClass(dir_ID, '09-041')
-    # print(dir_ID)
-    # print(addClass(dir_ID, '09-041', 18))
-    # print(addClass(dir_ID, '09-042', 12))
-    # print(addPlanUni(dir_ID, 'алгебра и геометрия', "+", "+", 36, 54, 0))
+    # print(addDirection(course_ID, 'ПИ'))
+    # print(addDirection(course_ID, 'ПМИ'))
+    # print(addDirection(course_ID, 'ИСТ'))
+
+    # добавление информации про направления
+    dir_ID = getDirection(course_ID, 'ПИ')
+    # print(addClass(dir_ID, '09-051', 20))
+    # print(addClass(dir_ID, '09-052', 30))
+
+    # добавление информации про учебный план
+    # print(addPlanUni(dir_ID, 'Программная инженерия', "+", "+", 36, 18, 0))
+    # print(addPlanUni(dir_ID, 'Базы данных', "+", "+", 36, 36, 0))
+    # print(addPlanUni(dir_ID, 'Дискретная математика', "+", "+", 36, 18, 0))
+    # print(addPlanUni(dir_ID, 'Анализ сложных систем', "+", "+", 36, 36, 0))
+    # print(addPlanUni(dir_ID, 'Математический анализ', "+", "+", 36, 54, 0))
+    # print(addPlanUni(dir_ID, 'Информационная безопасность', "+", "+", 36, 36, 0))
+    # print(addPlanUni(dir_ID, 'Информационные технологии', "+", "+", 36, 18, 0))
+
+    # добавление информации про кафедры
     # print(addCafedra('КСАИТ', workID))
+
+    class_ID = getClass(dir_ID, '09-051')
     cafedra_ID = getCafedraID('КСАИТ', workID)
+
+
     # print(addTeacher('Андрианова Анастасия Александровна', cafedra_ID))
+    # print(addTeacher('Васильев Александр Валерьевич', cafedra_ID))
+    # print(addTeacher('Асхатов Радик Мухаметгалеевич', cafedra_ID))
+    # print(addTeacher('Еникеева Разиль Радикович', cafedra_ID))
+
+
     teacher_ID = getTeacherID('Андрианова Анастасия Александровна', cafedra_ID)
     # print(addTeacherClassesDir(teacher_ID, dir_ID, 'Программная инженерия'))
-    # print(class_ID)
     # print(addTeacherClassesClass(teacher_ID, class_ID, 'Программная инженерия'))
+ 
     type_place_ID = getTypePlaceID('practic')
-    week_ID = getWeekID('понедельник')
-    print(type_place_ID, week_ID, workID)
-    print(addPlaceUni(workID, '808', week_ID, type_place_ID, 30, '8:00', '16:00'))
+    week_ID = getWeekID('вторник')
+    # print(addPlaceUni(workID, '1010', week_ID, type_place_ID, 40, '8:00', '16:00'))
+    # print(addPlaceUni(workID, '1012', week_ID, type_place_ID, 30, '8:00', '16:00'))
+    # print(addPlaceUni(workID, '1206', week_ID, type_place_ID, 35, '8:00', '16:00'))
+    # print(addPlaceUni(workID, '802', week_ID, type_place_ID, 35, '8:00', '16:00'))
+
+    grafic_arr = getGrafic(workID)
+    grafic_ID = grafic_arr[0][0]
+    place_ID = 20
+    teach_ID = 3
+    
+    # arrs = addScheduleUniDIR(workID, grafic_ID, place_ID, teach_ID, dir_ID, week_ID, '2023-02-13', '2023-05-29')
+    # print(arrs)
+
+    print(getSchedule(workID))
